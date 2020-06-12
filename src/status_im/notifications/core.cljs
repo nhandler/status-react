ef69836deda56b944a6790654b859281235c6783
Andrey Shovkoplyas <motor4ik@gmail.com>
2020-06-12 10:54:28 +0200

    ios pn

--- src/status_im/notifications/core.cljs
+++ src/status_im/notifications/core.cljs
@@ -2,7 +2,27 @@
   (:require [re-frame.core :as re-frame]
             [taoensso.timbre :as log]
             [status-im.utils.fx :as fx]
-            [status-im.native-module.core :as status]))
+            [status-im.native-module.core :as status]
+            ["react-native-push-notification" :as rn-pn]
+            [quo.platform :as platform]
+            [status-im.utils.utils :as utils]
+            [status-im.ui.components.react :as react]))
+
+(defn enable-ios-notifications []
+  (.configure
+   ^js rn-pn
+   (clj->js {:onRegister (fn [token-data]
+                           ;;TODO register token in status pn node send waku message
+                           (let [token (.-token ^js token-data)]
+                             (utils/show-popup nil
+                                               (str "Token " token)
+                                               #())
+                             (react/copy-to-clipboard token)
+                             (println "TOKEN " token)))})))
+
+(defn disable-ios-notifications []
+  ;;TODO remove token from status pn node, send waku message
+  (.abandonPermissions ^js rn-pn))
 
 ;; FIXME: Repalce with request permission from audio messages PR lib
 (re-frame/reg-fx
@@ -23,9 +43,13 @@
 (re-frame/reg-fx
  ::enable
  (fn [_]
-   (status/enable-notifications)))
+   (if platform/android?
+     (status/enable-notifications)
+     (enable-ios-notifications))))
 
 (re-frame/reg-fx
  ::disable
  (fn [_]
-   (status/disable-notifications)))
+   (if platform/android?
+     (status/disable-notifications)
+     (disable-ios-notifications))))
